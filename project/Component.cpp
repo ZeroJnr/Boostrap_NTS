8d28736703732aa8db794947cc1751d987643c9a
Liam <liam.esteffe2@gmail.com>
2021-02-08 12:14:23 +0100

    mv Project

---
+++ project/Component.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** Project Name
+** File description:
+** Component Boostrap CPP
+*/
+
+Component::Component()
+{
+}
+
+Component::~Component()
+{
+}
---
+++ project/Component.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2021
+** Project Name
+** File description:
+** Component Boostrap HPP
+*/
+
+#pragma once
+#include <iostream>
+
+
+class Component public : IComponent {
+	public:
+		Component();
+		~Component();
+	protected:
+	private:
+}
